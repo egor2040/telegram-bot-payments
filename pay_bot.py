b74e564b348736ffba173b21dde18a0cdfcf0fc3
egor2040 <egorruzanov2040@gmail.com>
2025-05-22 22:19:21 +0600

    Update pay_bot.py

--- pay_bot.py
+++ pay_bot.py
@@ -3,7 +3,7 @@ from telegram import Update, LabeledPrice
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, PreCheckoutQueryHandler, CallbackContext
 
 # Вставьте сюда ваш токен
-TOKEN = '7007004147:AAG30MpOgpa7ZQCl9SYJjS9WjqwRMslTnwA'
+TOKEN = os.getenv("BOT_TOKEN")
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
